8e94d22d5289a4dc8cef5f8ab12e9ef3218fae6b
Maxime Cliche <mcliche91@gmail.com>
2018-12-04 20:41:34 -0500

    Removed files from repo

--- dist/constValue.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.MAX_PORT = 65535;
-//# sourceMappingURL=constValue.js.map
--- dist/main.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Server_1 = require("./Server");
-var dotenv = require("dotenv");
-dotenv.config();
-var server = new Server_1.Server();
-//# sourceMappingURL=main.js.map
